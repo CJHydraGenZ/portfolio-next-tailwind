c864c5246ba7368f5d7194317e5386cd9323ff62
Cahya <cahyasaga321@gmail.com>
2022-06-26 18:01:37 +0800

    addd

--- pages/work.js
+++ pages/work.js
@@ -1,7 +1,18 @@
 import React from "react";
 import { Element } from "react-scroll";
+import Image from "next/image";
+
 const work = () => {
-  return <div>work</div>;
+  return (
+    <Element>
+      <div className="w-full my-20 h-auto flex flex-col justify-center items-center">
+        <p className="text-sm uppercase text-gray-400">Portfolio</p>
+        <h2 className="text-indigo-900 text-6xl font-bold text-center">
+          All Creative Works
+        </h2>
+      </div>
+    </Element>
+  );
 };
 
 export default work;
