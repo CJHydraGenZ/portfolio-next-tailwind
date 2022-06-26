c427eda1af2d970bbea6c5a5073315179a2d26bf
Cahya <cahyasaga321@gmail.com>
2022-06-26 18:09:51 +0800

    work

--- pages/work.js
+++ pages/work.js
@@ -10,6 +10,65 @@ const work = () => {
         <h2 className="text-indigo-900 text-6xl font-bold text-center">
           All Creative Works
         </h2>
+
+        <div className="md:flex sm:flex-col md:flex-row justify-center items-center gap-10 mt-10">
+          <div className="rounded w-80 h-60 shadow-xl relative">
+            <Image
+              src="/images/1.png"
+              alt="one"
+              layout="fill"
+              objectFit="cover"
+              className="p-2 rounded cursor-pointer"
+            />
+          </div>
+          <div className="rounded w-80 h-60 shadow-xl relative">
+            <Image
+              src="/images/2.png"
+              alt="two"
+              layout="fill"
+              objectFit="cover"
+              className="p-2 rounded cursor-pointer"
+            />
+          </div>
+          <div className="rounded w-80 h-60 shadow-xl relative">
+            <Image
+              src="/images/3.png"
+              alt="three"
+              layout="fill"
+              objectFit="cover"
+              className="p-2 rounded cursor-pointer"
+            />
+          </div>
+        </div>
+        <div className="md:flex sm:flex-col md:flex-row justify-center items-start md:gap-10 sm:pb-10 mt-10">
+          <div className="rounded w-80 h-60 shadow-xl relative">
+            <Image
+              src="/images/1.png"
+              alt="one"
+              layout="fill"
+              objectFit="cover"
+              className="p-2 rounded cursor-pointer"
+            />
+          </div>
+          <div className="rounded w-80 h-60 shadow-xl relative">
+            <Image
+              src="/images/2.png"
+              alt="two"
+              layout="fill"
+              objectFit="cover"
+              className="p-2 rounded cursor-pointer"
+            />
+          </div>
+          <div className="rounded w-80 h-60 shadow-xl relative">
+            <Image
+              src="/images/3.png"
+              alt="three"
+              layout="fill"
+              objectFit="cover"
+              className="p-2 rounded cursor-pointer"
+            />
+          </div>
+        </div>
       </div>
     </Element>
   );
